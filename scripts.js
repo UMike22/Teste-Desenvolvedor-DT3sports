05456723083f6d1b8418d8b71958fe9af6247fa6
UMike22 <49347143+UMike22@users.noreply.github.com>
2020-12-09 16:41:31 -0300

    teste complete

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
+const target = document.querySelectorAll('[data-anime]')
+const animationClass = 'animate'
+const burger = document.querySelector('.burger')
+const overlay = document.querySelector('.overlay')
+const nav = document.querySelector('.HomeList')
+
 const navSlide = () => {
-    const burger = document.querySelector('.burger')
-    const overlay = document.querySelector('.overlay')
-    const nav = document.querySelector('.HomeList')
     burger.addEventListener('click', () => {
         nav.classList.toggle('HomeList-active')
     })
@@ -27,9 +30,8 @@ const debounce = function(func, wait, immediate) {
     };
   };
 
-const target = document.querySelectorAll('[data-anime]')
-const animationClass = 'animate'
-
+  /* FUNÇÃO  QUE ANIMA OS ELEMENTOS DO SECTION AO SCROLL */
+  
 function animeScroll(){
     const windowTop = window.pageYOffset + (window.innerHeight * 0.75)
     target.forEach(function(element){
@@ -48,3 +50,58 @@ window.addEventListener('scroll', debounce(function(){
     animeScroll()
 }, 200))
 }
+
+// FUNÇÃO QUE DEIXA O SCROLL COM LINK INTERNO SUAVE //
+
+const menuItems = document.querySelectorAll('a[href^="#"]')
+
+menuItems.forEach(item => {
+    item.addEventListener('click' , scrollToIdOnClick)
+})
+
+function scrollToIdOnClick(event){
+    event.preventDefault()
+    const to = getScrollTopByHref(event.target) - 50
+    
+    
+    ScrollToPosition(to)
+}
+function ScrollToPosition(to){
+    smoothScrollTo(0, to , 900)
+}
+
+function getScrollTopByHref(element){
+    const id = element.getAttribute('href')
+    return  document.querySelector(id).offsetTop
+}
+/**
+ * Smooth scroll animation
+ * @param {int} endX: destination x coordinate
+ * @param {int} endY: destination y coordinate
+ * @param {int} duration: animation duration in ms
+ */
+function smoothScrollTo(endX, endY, duration) {
+    const startX = window.scrollX || window.pageXOffset;
+    const startY = window.scrollY || window.pageYOffset;
+    const distanceX = endX - startX;
+    const distanceY = endY - startY;
+    const startTime = new Date().getTime();
+  
+    duration = typeof duration !== 'undefined' ? duration : 400;
+  
+    // Easing function
+    const easeInOutQuart = (time, from, distance, duration) => {
+      if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+      return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+    };
+  
+    const timer = setInterval(() => {
+      const time = new Date().getTime() - startTime;
+      const newX = easeInOutQuart(time, startX, distanceX, duration);
+      const newY = easeInOutQuart(time, startY, distanceY, duration);
+      if (time >= duration) {
+        clearInterval(timer);
+      }
+      window.scroll(newX, newY);
+    }, 1000 / 60); // 60 fps
+  };
